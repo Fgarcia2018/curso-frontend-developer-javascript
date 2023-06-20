91406ada9e9c6dd080d1b93f8e26d0e7dd43c490
Fernando García <andgarcia_24@hotmail.com>
2023-06-20 11:25:50 -0500

    Se agregan funcionalidades para que las opciones de la barra de menu no se interpongan entre si al momento de abrirlas. Se realiza mediaqueries para ocultar menus que no deben ser visibles en determinada pantalla.

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 const menuEmail=document.querySelector('.navbar-email')
 const submenuEmail=document.querySelector('.menu-email')
+const menuMobile=document.querySelector('.mobile-menu')
+const menuHamIcon=document.querySelector('.menu-icon')
+const menuCarritoCompras=document.querySelector('.navbar-shopping-cart')
+const aside=document.querySelector('.product-detail')
+
 menuEmail.addEventListener('click',intercambioMenuEmail)
+menuHamIcon.addEventListener('click',intercambioMenuMobile)
+menuCarritoCompras.addEventListener('click',intercambioMenuCarrito)
 
 
-function intercambioMenuEmail(){
-    submenuEmail.classList.toggle('menu-email')       
+function intercambioMenuEmail(){    
+    // se valida si el contenedor del carrito de compras contiene la clase inactive.  
+    const isAsideClosed=aside.classList.contains('inactive')   
+    // si el contenedor del carrito de compras no contiene la clase inactive, se le agrega. La misma lógica se aplica para las demás opciones    
+    if(!isAsideClosed){  
+        aside.classList.add('inactive')
+    }
+    submenuEmail.classList.toggle('inactive')     
+}
+
+function intercambioMenuMobile(){
+    const isAsideClosed=aside.classList.contains('inactive')   
+    if(!isAsideClosed){  
+        aside.classList.add('inactive')
+    }
+        menuMobile.classList.toggle('inactive')     
+}  
+
+function intercambioMenuCarrito(){   
+    const isMenuMobileClosed=menuMobile.classList.contains('inactive') 
+    const issubmenuEmailClosed=menuEmail.classList.contains('inactive') 
+
+    if(!isMenuMobileClosed || !issubmenuEmailClosed){  
+        menuMobile.classList.add('inactive')
+        submenuEmail.classList.add('inactive')
+    }
+        aside.classList.toggle('inactive')     
 }
